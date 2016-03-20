030e9b5eee1a5e9c2c02a3837bccfb77a5dfd644
ZEVs <zevs-86@yandex.ru>
2016-03-20 23:01:45 +0300

    preloader

--- CurrencyRatesSwift/ViewController.swift
+++ CurrencyRatesSwift/ViewController.swift
@@ -10,14 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    let url = NSURL(string: "http://www.banki.ru/products/currency/cash/yaroslavl~/")
+    let url = "http://www.banki.ru/products/currency/cash/yaroslavl~/"
     var bankNames: [String] = []
     var usdBuyValues: [Float] = []
     var usdSellValues: [Float] = []
     
     @IBOutlet weak var loadButton: UIButton!
     @IBOutlet weak var resultTableView: UITableView!
-   
+    @IBOutlet weak var alert: UIAlertController!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,37 +29,61 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // load html from url
+    func getHtmlFromUrl(url: String, callback: (data: NSData?) -> Void) {
+        if let url = NSURL(string: url) {
+            NSURLSession.sharedSession().dataTaskWithURL(url) {
+                (data, response, error) in
+                dispatch_async(dispatch_get_main_queue(), {
+                    callback(data: data)
+                })
+            }.resume()
+        }
+    }
+    
+    
+    // make float value from string
     internal func prepareFloatValue(str: String) -> Float {
         if !str.isEmpty {
-            //var str2: String
             var str = str.stringByReplacingOccurrencesOfString("\t", withString: "")
             str = str.stringByReplacingOccurrencesOfString("\n", withString: "")
             str = str.stringByReplacingOccurrencesOfString(",", withString: ".")
-            //NSLog("String value: \(str)")
             let result = Float(str)
-            //NSLog("Float value: \(result)")
             return result!
         }
         return 0
     }
     
+    // press load
     @IBAction func loadButtonPressed(sender: AnyObject) {
 
         //let html = try NSString(contentsOfURL: url!, usedEncoding: nil)
         
-        let data = NSData(contentsOfURL: url!)
-        let doc = TFHpple(HTMLData: data!)
+        alert = UIAlertController(title: "Loading data",
+            message: "Please wait...",
+            preferredStyle: .Alert)
+        self.presentViewController(alert, animated: true, completion:nil)
+        
+        getHtmlFromUrl(url, callback: parseHtmlData)
+
+    }
+    
+    // parse data and show it in table view 
+    func parseHtmlData(data: NSData?) {
+        
+        let doc = TFHpple(HTMLData: data)
+        
         var elements = doc.searchWithXPathQuery("//a[@class='font-bold']")
         for element in elements {
             //NSLog(element.content)
             bankNames += [element.content]
         }
-            
+        
         elements = doc.searchWithXPathQuery("//td[@data-currencies-code='usd' and @data-currencies-rate-buy]")
         for element in elements {
             let usdValue: String = String(element.content)
             let val = self.prepareFloatValue(usdValue)
-               
+            
             if val > 0 {
                 usdBuyValues.append(val)
                 //NSLog(String(val))
@@ -81,8 +105,8 @@ class ViewController: UIViewController {
             NSLog("\(bankNames[i]) buy: \(usdBuyValues[i]) sell: \(usdSellValues[i])")
         }
         
-        
-        NSLog("end");
+        alert.dismissViewControllerAnimated(true, completion: nil)
+
     }
 
 }
